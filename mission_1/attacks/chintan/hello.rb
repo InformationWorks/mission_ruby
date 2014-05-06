6d2e93ac04d8521bd38edfcbe182f5509faffd90
chintant <trivedi.chintan46@gmail.com>
2014-05-06 14:29:11 +0530

    make a code in command line argument

--- mission_1/attacks/chintan/hello.rb
+++ mission_1/attacks/chintan/hello.rb
@@ -1,6 +1,9 @@
-puts "enter the number a"
-a=gets.to_i
-puts "enter the number b"
-b=gets.to_i
-c=a+b
-print "the addition is",c
+#!/usr/bin/env ruby
+ARGV.each do|a|
+  puts "Argument: #{a}"
+end
+
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+
+puts "Result: " ,a+b
